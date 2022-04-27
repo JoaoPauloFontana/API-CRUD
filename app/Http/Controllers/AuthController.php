732d097ea64ac6080386a5610eacde803c51b28e
Shaiqna <jpfontana12@icloud.com>
2022-04-26 22:47:39 -0300

    added logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -69,6 +69,9 @@ class AuthController extends Controller
             'error' => ''
         ];
 
+        $user = $request->user();
+        $user->tokens()->delete();
+
         return $array;
     }
 }
