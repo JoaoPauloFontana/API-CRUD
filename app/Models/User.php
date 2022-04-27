b4174858de8e0bf0dfabb2730c8528ca58b425b2
Shaiqna <jpfontana12@icloud.com>
2022-04-26 22:00:46 -0300

    Added HasApiTokens path

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,12 +6,12 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Laravel\Sanctum\Contracts\HasApiTokens;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
-    
+
     protected $hidden = [
         'password'
     ];
